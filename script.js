a9cbea76c2bf1430f3a4e3c55699a62aa2913f56
Julian <JulianStahl@gmx.de>
2021-12-30 14:46:07 +0100

    edit description

--- script.js
+++ script.js
@@ -76,8 +76,8 @@ function showWeatherData(data) {
             <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
             <div class="other">
                 <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
-                <div class="temp">Nacht - ${day.temp.night}&#176; C</div>
-                <div class="temp">Tag - ${day.temp.day}&#176; C</div>
+                <div class="temp">Nacht  ${day.temp.night}&#176; C</div>
+                <div class="temp">Tag  ${day.temp.day}&#176; C</div>
             </div>
             `
 
@@ -86,8 +86,8 @@ function showWeatherData(data) {
             <div class="weather-forecast-item">
                 <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
                 <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <div class="temp">Nacht - ${day.temp.night}&#176; C</div>
-                <div class="temp">Tag - ${day.temp.day}&#176; C</div>
+                <div class="temp">Nacht  ${day.temp.night}&#176; C</div>
+                <div class="temp">Tag  ${day.temp.day}&#176; C</div>
             </div>
     
             `
